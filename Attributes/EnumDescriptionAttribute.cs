e06b58550a9ee56789bb56d898d7d31c4b13ca36
weitaolee <12666954@163.com>
2014-10-04 13:39:35 +0800

    enum attr update

--- Attributes/EnumDescriptionAttribute.cs
+++ Attributes/EnumDescriptionAttribute.cs
@@ -12,16 +12,14 @@ namespace FC.Framework
     public class EnumDescriptionAttribute : Attribute
     {
         private string _strDescription;
-        private string[] _supportCultures;
         /// <summary>
         /// 构造函数
         /// </summary>
         /// <param name="strEnumDescription">枚举描述</param>
         /// <param name="supportCultures">支持的语言文化（为空时支持所有语言）</param>
-        public EnumDescriptionAttribute(string strEnumDescription, params string[] supportCultures)
+        public EnumDescriptionAttribute(string strEnumDescription)
         {
             _strDescription = strEnumDescription;
-            _supportCultures = supportCultures;
         }
 
         /// <summary>
@@ -29,16 +27,11 @@ namespace FC.Framework
         /// </summary>
         public string Description { get { return _strDescription; } }
         /// <summary>
-        /// 支持的文化
-        /// </summary>
-        public string[] SupportCultures { get { return _supportCultures; } }
-
-        /// <summary>
         /// 获取枚举的描述
         /// </summary>
         /// <param name="enumObj"></param>
         /// <returns></returns>
-        public static string GetEnumDescription(Enum enumObj, string culture = "")
+        public static string GetEnumDescription(Enum enumObj)
         {
             string des = string.Empty;
             System.Reflection.FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
@@ -48,35 +41,11 @@ namespace FC.Framework
                 if (attribArray.Length > 0)
                 {
                     var attr = attribArray[0] as EnumDescriptionAttribute;
-                    if (attr.SupportCultures == null || attr.SupportCultures.Count() == 0 || string.IsNullOrEmpty(culture) || attr.SupportCultures.Contains(culture))
-                        des = attr.Description;
+                    des = attr.Description;
                 }
             }
 
             return des;
-        }
-
-        /// <summary>
-        /// 获取枚举的描述
-        /// </summary>
-        /// <param name="enumObj"></param>
-        /// <returns></returns>
-        public static bool TryGetEnumDescription(Enum enumObj, string culture, out string description)
-        {
-            description = string.Empty;
-            System.Reflection.FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
-            if (fieldInfo != null)
-            {
-                object[] attribArray = fieldInfo.GetCustomAttributes(false);
-                if (attribArray.Length > 0)
-                {
-                    var attr = attribArray[0] as EnumDescriptionAttribute;
-                    if (attr.SupportCultures == null || attr.SupportCultures.Count() == 0 || string.IsNullOrEmpty(culture) || attr.SupportCultures.Contains(culture))
-                        description = attr.Description;
-                }
-            }
-
-            return string.IsNullOrEmpty(description);
         }
     }
 }
--- Extensions/EnumExtension.cs
+++ Extensions/EnumExtension.cs
@@ -12,21 +12,9 @@ namespace FC.Framework
         /// </summary>
         /// <param name="sourceEnum"></param>
         /// <returns>description</returns>
-        public static string GetDescription(this Enum sourceEnum, string cultrue = "ALL")
+        public static string GetDescription(this Enum sourceEnum)
         {
-            return EnumDescriptionAttribute.GetEnumDescription(sourceEnum, cultrue);
+            return EnumDescriptionAttribute.GetEnumDescription(sourceEnum);
         }
-
-        /// <summary>
-        ///  获得枚举的描述属性值
-        /// </summary>
-        /// <param name="sourceEnum"></param>
-        /// <param name="cultrue">语言文化</param>
-        /// <returns>if has description</returns>
-        public static bool TryGetDescription(this Enum sourceEnum, string cultrue, out string description)
-        {
-            return EnumDescriptionAttribute.TryGetEnumDescription(sourceEnum, cultrue, out description);
-        }
-
     }
 }
