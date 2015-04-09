acd4cffd373e98f8f50b6d0a55e9ca284bf1c1f2
weitaolee <12666954@qq.com>
2015-04-09 21:44:15 +0800

    add memcached

---
+++ Plusins/Memcached/FCFrameworkMemcachedExtension.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FC.Framework.Memcached
+{
+    public static class DFrameworkMemcachedExtension
+    {
+        /// <summary>
+        ///  CouchBase cache plusins
+        /// </summary>
+        /// <param name="framework"></param>
+        /// <returns>FCFramework</returns>
+        public static FCFramework UseMemcached(this FCFramework framework, string memcacheServerIp, string zone = "", string ocsUser = "", string ocsPassword = "")
+        {
+            IoC.Register<ICache>(new Memcache(memcacheServerIp, zone, ocsUser, ocsPassword));
+
+            return framework;
+        }
+    }
+}
